58d99264097316e803de26090d8b2e672ef9ba95
Dmitry Seleznev <selezards@yandex.ru>
2021-04-19 13:45:02 +0300

    добавил модель персонажей

--- src/models/person.py
+++ src/models/person.py
@@ -1,6 +1,6 @@
 import orjson
 import uuid
-from typing import Optional
+from typing import List, Optional
 
 # Используем pydantic для упрощения работы при перегонке данных из json в объекты
 from pydantic import BaseModel
@@ -9,7 +9,11 @@ def orjson_dumps(v, *, default):
     # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
     return orjson.dumps(v, default=default).decode()
 
-class Genre(BaseModel):
+
+
+class Person(BaseModel):
     id: uuid.UUID
-    name: str
-    description: Optional[str] = ''
+    full_name: str
+    role: str
+    film_ids: Optional[List[uuid.UUID]] # возможно тут будет косяк, проверить!
+    
